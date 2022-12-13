da7130ea1434a66f4f45ebccdca3fe36a047d234
Loki157 <lokesh2001sankar@gmail.com>
2022-12-14 03:41:02 +0530

    home la panathu

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
 import React from "react";
 import CardTitle1 from "./components/CardTitle1";
 import { useFormik } from "formik";
+import "../src/App.css";
+import teamwork from "./assets/teamwork.jpg";
+import { Grid } from "@mui/material";
+import { Box } from "@mui/system";
+import Scrollbars from "react-custom-scrollbars/lib/Scrollbars";
+// const useStyles = makeStyles({
+//   root: {
+//     position: "sticky",
+//     top: "1rem",
+//     minWidth: "275",
+//   },
+// });
 function App() {
   const formik = useFormik({});
+  //  const classes = useStyles();
   return (
-    <div>
-      <CardTitle1 />
-    </div>
+    <React.Fragment>
+      <div className="all">
+        <div className="imageback">
+          <div className="image">
+            {" "}
+            <img src={teamwork} />
+          </div>
+        </div>
+        <div className="commonCard">
+          <Scrollbars>
+            <div className="component">
+              <Box>
+                <CardTitle1 />
+              </Box>
+            </div>
+          </Scrollbars>
+        </div>
+      </div>
+      {/* <CardTitle1 /> */}
+    </React.Fragment>
   );
 }
 
--- src/components/CardTitle1.jsx
+++ src/components/CardTitle1.jsx
@@ -13,10 +13,15 @@ import {
   Select,
   MenuItem,
   Button,
+  Typography,
+  makeStyles,
 } from "@mui/material";
 import * as yup from "yup";
 import { useFormik } from "formik";
+
 import "../Styles/CardTitle1.css";
+import { Box } from "@mui/system";
+
 function CardTitle1() {
   const formik = useFormik({
     initialValues: {
@@ -24,168 +29,282 @@ function CardTitle1() {
       lastName: "",
       radio: "",
       age: Number,
-      passOutYear: Number,
+      passOutYear: "",
       Course: "",
       percentage: Number,
     },
-    validationSchema: {
+    validationSchema: yup.object({
       firstName: yup
         .string()
         .max(20, "Maximum 20 Characters allowed")
         .min(3, "Minimum 4 characters")
         .required("FirstName is Must"),
 
-      LastName: yup
+      lastName: yup
         .string()
         .max(20, "Maximum 20 Characters allowed")
         .min(3, "Minimum 4 characters")
         .required("Last Name Is Must"),
       radio: yup.string().required("Select Gender"),
       age: yup
-        .number()
+        .string()
         .max(100, "Maximum 100 allowed")
 
         .required("Must Enter Age"),
-      passOutYear: yup.number().required("Must Enter Age"),
+      passOutYear: yup.string().required("Enter Passed Out Year"),
       Course: yup.string().required("Enter Course"),
       percentage: yup
-        .number()
+        .string()
         .max(100, "Max 100% is Required")
         .required("Enter Percentage"),
+    }),
+    onSubmit: (value) => {
+      console.log("value", value);
     },
-    onSubmit: (value) => console.log("value", value),
   });
   return (
-    <div className="whole">
-      <Container fixed sx={{ height: "100vh", backgroundColor: "#f5f5f5" }}>
+    <>
+      <Container fixed>
+        <h2>Employee Register</h2>
         <form onSubmit={formik.handleSubmit}>
-          <Card sx={{ height: "400px" }}>
-            <Container maxWidth="sm">
-              <Grid container>
-                <Grid container md={12}>
-                  <Grid item md={6} sx={{ my: 2 }}>
+          <Box display="flex" flexDirection="column">
+            <Card
+              sx={{ height: "400px", backgroundColor: "#afcb64 !important" }}
+            >
+              <Container fixed>
+                <Grid container>
+                  <Grid item md={12}>
+                    <h2>Personal Details</h2>
+                  </Grid>
+                  <Grid container md={12}>
+                    <Grid item md={6} sx={{ my: 2 }}>
+                      <FormControl>
+                        <FormLabel id="Firstname" sx={{ fontWeight: "bold" }}>
+                          First Name
+                        </FormLabel>
+                        <TextField
+                          name="firstName"
+                          value={formik.values.firstName}
+                          onChange={formik.handleChange}
+                          onBlur={formik.handleBlur}
+                          placeholder="Enter First Name"
+                        />
+                        {formik.errors.firstName && formik.touched.firstName ? (
+                          <Typography
+                            variant="p"
+                            sx={{ color: "crimson", fontSize: "15px" }}
+                          >
+                            {formik.errors.firstName}
+                          </Typography>
+                        ) : null}
+                      </FormControl>
+                    </Grid>
+                    <Grid item md={6} sx={{ my: 2 }}>
+                      <FormControl>
+                        <FormLabel id="LastName" sx={{ fontWeight: "bold" }}>
+                          Last Name
+                        </FormLabel>
+                        <TextField
+                          name="lastName"
+                          placeholder="Enter Last Name"
+                          value={formik.values.lastName}
+                          onChange={formik.handleChange}
+                          onBlur={formik.handleBlur}
+                        />
+                        {formik.errors.lastName && formik.touched.lastName ? (
+                          <Typography
+                            variant="p"
+                            sx={{ color: "crimson", fontSize: "15px" }}
+                          >
+                            {formik.errors.lastName}
+                          </Typography>
+                        ) : null}
+                      </FormControl>
+                    </Grid>
+                  </Grid>
+                  <Grid item md={12} sx={{ my: 2 }}>
                     <FormControl>
-                      <FormLabel id="Firstname">First Name</FormLabel>
-                      <TextField
-                        name="firstName"
-                        value={formik.values.firstName}
+                      <FormLabel
+                        id="demo-row-radio-buttons-group-label"
+                        sx={{ fontWeight: "bold" }}
+                      >
+                        Select Gender
+                      </FormLabel>
+                      <RadioGroup
+                        row
+                        aria-labelledby="demo-row-radio-buttons-group-label"
+                        name="radio"
+                        value={formik.values.radio}
                         onChange={formik.handleChange}
                         onBlur={formik.handleBlur}
-                        placeholder="Enter First Name"
-                      />
+                      >
+                        <FormControlLabel
+                          value="female"
+                          control={<Radio />}
+                          label="Female"
+                        />
+                        <FormControlLabel
+                          value="male"
+                          control={<Radio />}
+                          label="Male"
+                        />
+                        <FormControlLabel
+                          value="other"
+                          control={<Radio />}
+                          label="Other"
+                        />
+                      </RadioGroup>
+                      {formik.errors.radio && formik.touched.radio ? (
+                        <Typography
+                          variant="p"
+                          sx={{ color: "crimson", fontSize: "15px" }}
+                        >
+                          {formik.errors.radio}
+                        </Typography>
+                      ) : null}
                     </FormControl>
                   </Grid>
-                  <Grid item md={6} sx={{ my: 2 }}>
+                  <Grid item>
                     <FormControl>
-                      <FormLabel id="LastName">Last Name</FormLabel>
+                      <FormLabel id="age" sx={{ fontWeight: "bold" }}>
+                        {" "}
+                        Age
+                      </FormLabel>
                       <TextField
-                        name="lastName"
-                        placeholder="Enter Last Name"
-                        value={formik.values.lastName}
+                        name="age"
+                        placeholder="Enter Age"
+                        variant="standard"
+                        value={formik.values.age}
                         onChange={formik.handleChange}
                         onBlur={formik.handleBlur}
                       />
+                      {formik.errors.age && formik.touched.age ? (
+                        <Typography
+                          variant="p"
+                          sx={{ color: "crimson", fontSize: "15px" }}
+                        >
+                          {formik.errors.age}
+                        </Typography>
+                      ) : null}
                     </FormControl>
                   </Grid>
                 </Grid>
-                <Grid item md={12} sx={{ my: 2 }}>
-                  <FormControl>
-                    <FormLabel id="demo-row-radio-buttons-group-label">
-                      Select Gender
-                    </FormLabel>
-                    <RadioGroup
-                      row
-                      aria-labelledby="demo-row-radio-buttons-group-label"
-                      name="radio"
-                      value={formik.values.radio}
-                      onChange={formik.handleChange}
-                      onBlur={formik.handleBlur}
+              </Container>
+            </Card>
+
+            <Card sx={{ my: 2, height: 300 }}>
+              <h2 style={{ textAlign: "center" }}>Education Details</h2>
+              <Button>Add Education</Button>
+              <Box
+                sx={{
+                  my: 2,
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-around",
+                }}
+              >
+                <FormControl sx={{ width: 200 }}>
+                  <InputLabel
+                    id="demo-simple-select-label"
+                    sx={{ fontWeight: "bold" }}
+                  >
+                    Passed Out Year
+                  </InputLabel>
+                  <Select
+                    name="passOutYear"
+                    id="demo-simple-select"
+                    placeholder="Select Year"
+                    value={formik.values.passOutYear}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+
+                  {formik.errors.passOutYear && formik.touched.passOutYear ? (
+                    <Typography
+                      variant="p"
+                      sx={{ color: "crimson", fontSize: "15px" }}
                     >
-                      <FormControlLabel
-                        value="female"
-                        control={<Radio />}
-                        label="Female"
-                      />
-                      <FormControlLabel
-                        value="male"
-                        control={<Radio />}
-                        label="Male"
-                      />
-                      <FormControlLabel
-                        value="other"
-                        control={<Radio />}
-                        label="Other"
-                      />
-                    </RadioGroup>
-                  </FormControl>
-                </Grid>
-                <Grid item>
-                  <FormControl>
-                    <FormLabel id="age"> Age</FormLabel>
-                    <TextField
-                      name="age"
-                      placeholder="Enter Age"
-                      variant="standard"
-                      value={formik.values.age}
-                      onChange={formik.handleChange}
-                      onBlur={formik.handleBlur}
-                    />
-                  </FormControl>
-                </Grid>
-              </Grid>
-            </Container>
-          </Card>
-          <Card sx={{ my: 2, height: 200 }}>
-            <Container sx={{ my: 2 }}>
-              <FormControl sx={{ width: 200 }}>
-                <InputLabel id="demo-simple-select-label">Age</InputLabel>
-                <Select
-                  name="passOutYear"
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  label="Age"
-                  value={formik.values.passOutYear}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                >
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-              </FormControl>
-              <FormControl sx={{ width: 200 }}>
-                <InputLabel id="demo-simple-select-label">Age</InputLabel>
-                <Select
-                  name="Course"
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  label="Age"
-                  value={formik.values.Course}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                >
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-              </FormControl>
-              <TextField
-                type="number"
-                name="percentage"
-                label="Percentage"
+                      {formik.errors.passOutYear}
+                    </Typography>
+                  ) : null}
+                </FormControl>
+                <FormControl sx={{ width: 200 }}>
+                  <InputLabel
+                    id="demo-simple-select-label"
+                    sx={{ fontWeight: "bold" }}
+                  >
+                    Course
+                  </InputLabel>
+                  <Select
+                    name="Course"
+                    id="demo-simple-select"
+                    placeholder="Select Course"
+                    value={formik.values.Course}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                  {formik.errors.Course && formik.touched.Course ? (
+                    <Typography
+                      variant="p"
+                      sx={{ color: "crimson", fontSize: "15px" }}
+                    >
+                      {formik.errors.Course}
+                    </Typography>
+                  ) : null}
+                </FormControl>
+                <FormControl>
+                  <TextField
+                    type="number"
+                    name="percentage"
+                    placeholder="Enter percentage (%)"
+                    variant="standard"
+                    value=""
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                  {formik.errors.percentage && formik.touched.percentage ? (
+                    <Typography
+                      variant="p"
+                      sx={{ color: "crimson", fontSize: "15px" }}
+                    >
+                      {formik.errors.percentage}
+                    </Typography>
+                  ) : null}
+                </FormControl>
+              </Box>
+
+              <Button
                 variant="standard"
-                value={formik.values.percentage}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-              />
-              <Button variant="standard" type="submit">
+                type="submit"
+                sx={{
+                  my: 2,
+                  backgroundColor: "rgb(87, 73, 209)",
+                  color: "white",
+                  "&:hover": { backgroundColor: "rgb(38, 21, 187)" },
+                  textAlign: "center",
+                  padding: "10px",
+                  width: "100%",
+                  // display: "flex",
+                  // flexDirection: "column",
+                  // alignItems: "center",
+                  // justifyContent: "center",
+                }}
+              >
                 Submit{" "}
               </Button>
-            </Container>
-          </Card>
+            </Card>
+          </Box>
         </form>
       </Container>
-    </div>
+    </>
   );
 }
 
